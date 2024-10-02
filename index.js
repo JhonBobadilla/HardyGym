152bda32d7125cfe06651a1e8a1a265da5f06eaf
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2024-10-02 17:07:19 -0500

    070

---
+++ index.js
@@ -0,0 +1,35 @@
+function sus(s)
+{
+
+    let edad = parseInt(prompt("ingresa tu edad")); 
+    
+    if (edad < 18) {
+
+        alert ("Debes tener más de 18 años");
+        window.location.href = "./pages/contactenos.html";
+    }
+
+    else if (edad >= 18) {
+
+   alert ("Puedes inscribirte")
+   window.location.href = 'https://www.google.com/';
+   
+   }
+
+    else if (edad != edad) {
+
+    alert ("Debes ingresar un valor")
+            
+    } 
+
+}
+
+
+
+
+
+
+
+
+
+
