50b83efc551a373ccbad03f8a38c426ca4110d48
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2024-11-07 15:55:41 -0500

    0104

--- index.js
+++ index.js
@@ -52,17 +52,13 @@ function acceptTerms() {
 
 /* JS BARRA DE PROGRESO */
 
+let progress = 0; const progressBar = document.getElementById('progress-bar'); function advanceProgress() { if (progress < 100) { progress += 34; // Ajusta el valor del incremento según sea necesario
+updateProgressBar(progress); } } function decreaseProgress() { if (progress > 0) { progress -= 34; // Ajusta el valor del decremento según sea necesario 
+updateProgressBar(progress); } } function updateProgressBar(value) { if (value > 100) { value = 100; } else if (value < 0) { value = 0; } progressBar.style.width = `${value}%`; if (value <= 34) { progressBar.style.backgroundColor = '#ff0000'; // Red 
+  } else if (value <= 68) { progressBar.style.backgroundColor = '#f39c12'; // Orange 
+    } else { progressBar.style.backgroundColor = '#7ed957'; // Green 
+      } }
 
-let progress = 0; 
-const progressBar = document.getElementById('progress-bar'); 
-function advanceProgress() { if (progress < 100) { progress += 33.34; // Avanza en tres pasos
-   updateProgressBar(progress); } } 
-   function decreaseProgress() { 
-    if (progress > 0) { progress -= 33.34; // Retrocede en tres pasos 
-    updateProgressBar(progress); } } 
-    function updateProgressBar(value) { progressBar.style.width = `${value}%`; 
-    if (value <= 33.34) { progressBar.style.backgroundColor = 'red'; } 
-    else if (value <= 66.65) { progressBar.style.backgroundColor = 'orange'; } else if (value <= 100) { progressBar.style.backgroundColor = 'green'; } }
 
 
 
