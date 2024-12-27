f2a7038d9fd7b9370e28676f1cc1fac06a114fb1
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2024-12-27 16:43:20 -0500

    0157

--- progress.js
+++ progress.js
@@ -34,11 +34,27 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const videos = [
-            'progress-bar-1', 'progress-bar-2', 'progress-bar-3', 'progress-bar-4',
-            'progress-bar-5', 'progress-bar-6', 'progress-bar-7', 'progress-bar-8',
-            'progress-bar-9', 'progress-bar-10', 'progress-bar-11', 'progress-bar-12',
-            'progress-bar-13', 'progress-bar-14', 'progress-bar-15', 'progress-bar-16',
-            'progress-bar-17', 'progress-bar-18', 'progress-bar-19', 'progress-bar-20',
+            
+            //PILATES
+            'progress-bar-1', 'progress-bar-2', 'progress-bar-3', 'progress-bar-4', 'progress-bar-5', 'progress-bar-6', 'progress-bar-7', 'progress-bar-8', 'progress-bar-9', 'progress-bar-10', 'progress-bar-11', 'progress-bar-12',
+            'progress-bar-13', 'progress-bar-14', 'progress-bar-15', 'progress-bar-16', 'progress-bar-17', 'progress-bar-18', 'progress-bar-19', 'progress-bar-20',
+
+            //FUNCIONAL
+            'progress-bar-21', 'progress-bar-22', 'progress-bar-23', 'progress-bar-24', 'progress-bar-25', 'progress-bar-26', 'progress-bar-27', 'progress-bar-28', 'progress-bar-29', 'progress-bar-30', 'progress-bar-31', 'progress-bar-32',
+            'progress-bar-33', 'progress-bar-34', 'progress-bar-35', 'progress-bar-36', 'progress-bar-37', 'progress-bar-38', 'progress-bar-39', 'progress-bar-40',
+
+            //PAUSAS_ACTIVAS
+            'progress-bar-41', 'progress-bar-42', 'progress-bar-43', 'progress-bar-44', 'progress-bar-45', 'progress-bar-46', 'progress-bar-47', 'progress-bar-48', 'progress-bar-49', 'progress-bar-50', 'progress-bar-51', 'progress-bar-52',
+            'progress-bar-53', 'progress-bar-54', 'progress-bar-55', 'progress-bar-56', 'progress-bar-57', 'progress-bar-58', 'progress-bar-59', 'progress-bar-60',
+
+            //RUMBA
+            'progress-bar-61', 'progress-bar-62', 'progress-bar-63',   'progress-bar-64', 'progress-bar-65', 'progress-bar-66',     'progress-bar-67', 'progress-bar-68', 'progress-bar-69',     'progress-bar-70', 'progress-bar-71', 'progress-bar-72',
+            'progress-bar-73', 'progress-bar-74', 'progress-bar-75',     'progress-bar-76', 'progress-bar-77', 'progress-bar-78',    'progress-bar-79', 'progress-bar-80',
+
+            //COMBAT
+            'progress-bar-81', 'progress-bar-82', 'progress-bar-83',    'progress-bar-84', 'progress-bar-85', 'progress-bar-86',     'progress-bar-87', 'progress-bar-88', 'progress-bar-89',     'progress-bar-90', 'progress-bar-91', 'progress-bar-92',
+            'progress-bar-93', 'progress-bar-94', 'progress-bar-95',     'progress-bar-96', 'progress-bar-97', 'progress-bar-98',    'progress-bar-99', 'progress-bar-100'
+        
         ];
 
         // Cargar el progreso de cada video al iniciar
