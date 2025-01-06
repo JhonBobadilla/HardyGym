58b2a3b808445b87b9d60bdb1b286ac3d11e234b
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2025-01-06 18:20:21 -0500

    0204

--- progress.js
+++ progress.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const INCREMENT = 34; // Definimos el tamaño del incremento/decremento
     const token = localStorage.getItem('token'); // Obtener el token desde localStorage
 
-    // Asegúrate de que el token no sea nulo antes de hacer solicitudes
     if (!token) {
         console.error('Token no encontrado en localStorage.');
         return;
@@ -25,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(data => data.userId)
         .catch(error => {
             console.error('Error al obtener el userId:', error);
-            return null; // Manejo de error básico
+            return null;
         });
     }
 
@@ -36,23 +35,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         const videos = [
-            //PILATES
             'progress-bar-1', 'progress-bar-2', 'progress-bar-3', 'progress-bar-4', 'progress-bar-5', 'progress-bar-6', 'progress-bar-7', 'progress-bar-8', 'progress-bar-9', 'progress-bar-10', 'progress-bar-11', 'progress-bar-12',
             'progress-bar-13', 'progress-bar-14', 'progress-bar-15', 'progress-bar-16', 'progress-bar-17', 'progress-bar-18', 'progress-bar-19', 'progress-bar-20',
-
-            //FUNCIONAL
             'progress-bar-21', 'progress-bar-22', 'progress-bar-23', 'progress-bar-24', 'progress-bar-25', 'progress-bar-26', 'progress-bar-27', 'progress-bar-28', 'progress-bar-29', 'progress-bar-30', 'progress-bar-31', 'progress-bar-32',
             'progress-bar-33', 'progress-bar-34', 'progress-bar-35', 'progress-bar-36', 'progress-bar-37', 'progress-bar-38', 'progress-bar-39', 'progress-bar-40',
-
-            //PAUSAS_ACTIVAS
             'progress-bar-41', 'progress-bar-42', 'progress-bar-43', 'progress-bar-44', 'progress-bar-45', 'progress-bar-46', 'progress-bar-47', 'progress-bar-48', 'progress-bar-49', 'progress-bar-50', 'progress-bar-51', 'progress-bar-52',
             'progress-bar-53', 'progress-bar-54', 'progress-bar-55', 'progress-bar-56', 'progress-bar-57', 'progress-bar-58', 'progress-bar-59', 'progress-bar-60',
-
-            //RUMBA
             'progress-bar-61', 'progress-bar-62', 'progress-bar-63', 'progress-bar-64', 'progress-bar-65', 'progress-bar-66', 'progress-bar-67', 'progress-bar-68', 'progress-bar-69', 'progress-bar-70', 'progress-bar-71', 'progress-bar-72',
             'progress-bar-73', 'progress-bar-74', 'progress-bar-75', 'progress-bar-76', 'progress-bar-77', 'progress-bar-78', 'progress-bar-79', 'progress-bar-80',
-
-            //COMBAT
             'progress-bar-81', 'progress-bar-82', 'progress-bar-83', 'progress-bar-84', 'progress-bar-85', 'progress-bar-86', 'progress-bar-87', 'progress-bar-88', 'progress-bar-89', 'progress-bar-90', 'progress-bar-91', 'progress-bar-92',
             'progress-bar-93', 'progress-bar-94', 'progress-bar-95', 'progress-bar-96', 'progress-bar-97', 'progress-bar-98', 'progress-bar-99', 'progress-bar-100'
         ];
@@ -105,12 +95,11 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
         value = Math.round(value);
-        if (value > 100) {
-            value = 100;
-        } else if (value < 0) {
-            value = 0;
-        }
         progressBar.style.width = `${value}%`;
+
+        console.log(`Actualizando progressBar a ${value}%`);
+
+        // Establecer el color correcto según el valor de progreso
         if (value === 0) {
             progressBar.style.backgroundColor = 'white';
         } else if (value <= 33) {
@@ -124,13 +113,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Función para guardar el progreso en la base de datos
     function saveProgress(userId, videoId, progress) {
-        console.log('Intentando guardar progreso:', { userId, videoId, progress }); // Log para verificar valores
+        console.log('Intentando guardar progreso:', { userId, videoId, progress });
 
         fetch(`${BASE_URL}/saveProgress`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token // Incluir el token de autenticación
+                'Authorization': 'Bearer ' + token
             },
             body: JSON.stringify({ userId, videoId, progress })
         })
@@ -152,15 +141,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         fetch(`${BASE_URL}/getProgress/${userId}/${videoId}`, {
             headers: {
-                'Authorization': 'Bearer ' + token // Incluir el token de autenticación
+                'Authorization': 'Bearer ' + token
             }
         })
         .then(response => response.json())
         .then(data => {
             const progress = data.progress;
+            console.log(`Progreso cargado: ${progress}`);
             const progressBar = document.getElementById(elementId);
             if (progressBar) {
-                console.log(`Progreso encontrado: ${progress}`);
+                console.log(`Actualizando progressBar con ID ${elementId} a ${progress}%`);
                 updateProgressBar(progressBar, progress);
             } else {
                 console.error(`Elemento progressBar con ID ${elementId} no encontrado`);
@@ -171,4 +161,5 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
 
