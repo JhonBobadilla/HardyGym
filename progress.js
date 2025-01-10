bef67e67f630690eda71ae398f47048ca9245ac4
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2025-01-10 15:17:25 -0500

    0248

--- progress.js
+++ progress.js
@@ -1,12 +1,17 @@
+// Cargar progreso inicial
 document.addEventListener('DOMContentLoaded', () => {
-    // Cargar progreso inicial
     fetch('/get-progress', {
         method: 'GET',
         headers: {
             'Authorization': `Bearer ${localStorage.getItem('token')}`
         }
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al cargar el progreso');
+            }
+            return response.json();
+        })
         .then(data => {
             data.forEach(({ video_id, progress }) => {
                 const progressBar = document.getElementById(video_id);
@@ -15,7 +20,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             });
         })
-        .catch(console.error);
+        .catch(error => {
+            console.error('Error al cargar el progreso:', error);
+        });
 });
 
 function updateProgressBar(progressBar, progress) {
@@ -25,58 +32,11 @@ function updateProgressBar(progressBar, progress) {
         progressBar.style.backgroundColor = 'red';
     } else if (progress === 66) {
         progressBar.style.backgroundColor = 'yellow';
-    } else if (progress === 99) {
-        progressBar.style.backgroundColor = '#7ed957';
-    } else if (progress === 100) {
+    } else if (progress >= 99) {
         progressBar.style.backgroundColor = '#7ed957';
     }
 }
 
-function advanceProgress(videoId) {
-    const progressBar = document.getElementById(videoId);
-    let currentWidth = parseInt(progressBar.style.width) || 0;
-
-    if (currentWidth === 0) {
-        currentWidth = 33;  // Primer clic, color rojo
-        updateProgressBar(progressBar, currentWidth);
-        saveProgress(videoId, currentWidth);
-    } else if (currentWidth === 33) {
-        currentWidth = 66;  // Segundo clic, color amarillo
-        updateProgressBar(progressBar, currentWidth);
-        saveProgress(videoId, currentWidth);
-    } else if (currentWidth === 66) {
-        currentWidth = 99;  // Tercer clic, color verde al 99%
-        updateProgressBar(progressBar, currentWidth);
-        saveProgress(videoId, currentWidth);
-    } else if (currentWidth === 99) {
-        currentWidth = 100;  // Cuarto clic, mantén verde al 100%
-        updateProgressBar(progressBar, currentWidth);
-        saveProgress(videoId, currentWidth);
-    }
-}
-
-function decreaseProgress(videoId) {
-    const progressBar = document.getElementById(videoId);
-    let currentWidth = parseInt(progressBar.style.width) || 0;
-
-    if (currentWidth > 0) {
-        currentWidth -= 33;
-        updateProgressBar(progressBar, currentWidth);
-        saveProgress(videoId, currentWidth);
-    }
-}
-
-function saveProgress(videoId, progress) {
-    fetch('/save-progress', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${localStorage.getItem('token')}`
-        },
-        body: JSON.stringify({ video_id: videoId, progress })
-    })
-        .catch(console.error);
-}
 
 
 
