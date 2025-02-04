52905e84893541e3914e0697dd6fc032f4d12457
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2025-02-04 14:41:32 -0500

    0444

---
+++ auth.js
@@ -0,0 +1,41 @@
+// auth.js
+
+const token = localStorage.getItem('token');
+const isFromPagoSuscripcion = document.referrer.includes('pago_suscripcion.html'); // Verifica si viene de la página de suscripción
+
+if (!token && !isFromPagoSuscripcion) {
+    window.location.href = '../public/index.html'; // Redirige a la página de inicio de sesión si no hay token y no viene de 'pago_suscripcion.html'
+} else {
+    if (token || isFromPagoSuscripcion) {
+        // Si tiene token o viene de 'pago_suscripcion.html', continua con la validación
+        if (token) {
+            fetch('/getUserId', {
+                headers: { 'Authorization': `Bearer ${token}` }
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('No autorizado');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Usuario autenticado:', data.userId);
+                document.getElementById('nombreUsuario').innerText = data.nombre; // Mostrar el nombre del usuario
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                window.location.href = '../public/index.html'; // Redirige a la página de inicio de sesión en caso de error
+            });
+        } else {
+            console.log('Acceso permitido desde pago_suscripcion.html');
+            document.getElementById('nombreUsuario').innerText = 'Invitado'; // Opción de usuario no autenticado, puede mostrar algo diferente
+        }
+    }
+}
+
+// Lógica para cerrar sesión
+document.getElementById('logoutButton').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir el comportamiento por defecto del enlace
+    localStorage.removeItem('token'); // Elimina el token del almacenamiento local
+    window.location.href = '../public/index.html'; // Redirige a la página de inicio de sesión
+});
