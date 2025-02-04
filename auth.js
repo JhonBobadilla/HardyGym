4edea7dea90371f4cab1479075ad17d0bfacdb95
JhonBobadilla <jhon.alexander.bobadilla@gmail.com>
2025-02-04 14:49:05 -0500

    0446

--- auth.js
+++ auth.js
@@ -1,22 +1,27 @@
 // auth.js
 
-// auth.js
+function logMessage(message) {
+    let logs = JSON.parse(localStorage.getItem('logs')) || [];
+    logs.push(message);
+    localStorage.setItem('logs', JSON.stringify(logs));
+    console.log(message);
+}
 
-console.log('Script de autenticación iniciado');
+logMessage('Script de autenticación iniciado');
 
 const token = localStorage.getItem('token');
 const isFromPagoSuscripcion = document.referrer.includes('pago_suscripcion.html'); // Verifica si viene de la página de suscripción
 
-console.log('Token:', token);
-console.log('Viene de pago_suscripcion.html:', isFromPagoSuscripcion);
+logMessage('Token: ' + token);
+logMessage('Viene de pago_suscripcion.html: ' + isFromPagoSuscripcion);
 
 if (!token && !isFromPagoSuscripcion) {
-    console.log('Redirigiendo a la página de inicio de sesión');
+    logMessage('Redirigiendo a la página de inicio de sesión');
     window.location.href = '../public/index.html'; // Redirige a la página de inicio de sesión si no hay token y no viene de 'pago_suscripcion.html'
 } else {
     if (token || isFromPagoSuscripcion) {
         // Si tiene token o viene de 'pago_suscripcion.html', continua con la validación
-        console.log('Validación permitida');
+        logMessage('Validación permitida');
         if (token) {
             fetch('/getUserId', {
                 headers: { 'Authorization': `Bearer ${token}` }
@@ -28,15 +33,15 @@ if (!token && !isFromPagoSuscripcion) {
                 return response.json();
             })
             .then(data => {
-                console.log('Usuario autenticado:', data.userId);
+                logMessage('Usuario autenticado: ' + data.userId);
                 document.getElementById('nombreUsuario').innerText = data.nombre; // Mostrar el nombre del usuario
             })
             .catch(error => {
-                console.error('Error en la autenticación:', error);
+                logMessage('Error en la autenticación: ' + error);
                 window.location.href = '../public/index.html'; // Redirige a la página de inicio de sesión en caso de error
             });
         } else {
-            console.log('Acceso permitido desde pago_suscripcion.html');
+            logMessage('Acceso permitido desde pago_suscripcion.html');
             document.getElementById('nombreUsuario').innerText = 'Invitado'; // Opción de usuario no autenticado, puede mostrar algo diferente
         }
     }
@@ -49,5 +54,6 @@ document.getElementById('logoutButton').addEventListener('click', function(event
     window.location.href = '../public/index.html'; // Redirige a la página de inicio de sesión
 });
 
-console.log('Script de autenticación finalizado');
+logMessage('Script de autenticación finalizado');
+
 
